c4a9c9f7f1ab7c95d3817e1751f41f57e7e33b64
Abhinav Niranjan <66178038+knackofabhinav@users.noreply.github.com>
2021-01-09 09:10:43 +0530

    added own server from heroku

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get Quote From API
 async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const proxyUrl = 'https://mighty-escarpment-32848.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
